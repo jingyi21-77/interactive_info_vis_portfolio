606393c0814ac7c7bdcac479ec683edfc56654f9
jingyi21-77 <jingyi21@uw.edu>
2026-02-17 16:28:04 -0800

    add hwk5 sketch test

---
+++ hwk5/hwk5/sketch.js
@@ -0,0 +1,22 @@
+let table;
+
+function preload() {
+  table = loadTable("./data/album_summary.csv", "csv", "header");
+}
+
+function setup() {
+  createCanvas(900, 200);
+  background(255);
+
+  fill(0);
+  textSize(16);
+  text("Loaded rows: " + table.getRowCount(), 20, 40);
+
+  if (table.getRowCount() > 0) {
+    const r0 = table.getRow(0);
+    text("First album: " + r0.get("album_clean"), 20, 70);
+    text("Energy: " + r0.get("Energy"), 20, 100);
+  }
+}
+
+function draw() {}
